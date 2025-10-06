fc8ffa6b3b3316d2bb8c9adf639bbad465b684d2
Avishkar1234 <avishkar1234567890@gmail.com>
2025-10-06 18:41:12 +0000

    chore: set up initial book grid layout and styling (static display)

--- index.js
+++ index.js
@@ -7,3 +7,32 @@ function Book(title, author, pages, isRead) {
     this.pages = pages;
     this.isRead = isRead;
 }
+
+function addBooks(title, author, pages, isRead){
+    const newBook = new Book(title, author, pages, isRead)
+    bookArr.push(newBook)
+}
+
+function displayBooks() {
+    const display = document.querySelector(".books")
+    display.innerHTML = ""
+
+    for(let book of bookArr) {
+        const card = document.createElement("div");
+        card.classList.add("book-card")
+        card.dataset.id = book.id;
+
+        card.innerHTML = `
+            <h2>${book.title}</h2>
+            <p><strong>Author: </strong>${book.author}</p>
+            <p><strong>Pages: </strong>${book.pages}</p>
+            <p><strong>Status:</strong> ${book.isRead ? 'Read ✅' : 'Not Read ❌'}</p>
+        `;
+        display.appendChild(card)
+    }
+}
+
+addBooks("Harry Potter", "J.K. Rowling", 400, true);
+addBooks("The Hobbit", "J.R.R. Tolkien", 310, false);
+
+displayBooks();
