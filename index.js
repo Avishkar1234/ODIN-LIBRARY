94177847e8d9225089af116dec2b5457cb6ae3f7
Avishkar1234 <avishkar1234567890@gmail.com>
2025-10-06 12:45:01 +0000

    feat: add Book constructor and myLibrary arrayS

---
+++ index.js
@@ -0,0 +1,9 @@
+const bookArr = []
+
+function Book(title, author, pages, isRead) {
+    this.id = crypto.randomUUID()
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
